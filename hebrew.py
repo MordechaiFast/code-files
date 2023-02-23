0eb3659304a150d6bde1d6123d0ed6197b8a8417
Mordechai Fast <95625541+MordechaiFast@users.noreply.github.com>
2023-02-23 13:45:47 +0000

    hebrew

---
+++ hebrew.py
@@ -0,0 +1,10 @@
+import unicodedata
+
+א_ב = "אבגדהוזחטיכלמנסעפצקרשתךםןףץ"
+#for letter in א_ב:
+for i in range(1424, 1536):
+    letter = chr(i)
+    try:
+        print(f"{letter}: {ord(letter)} \\u{ord(letter):04x} {unicodedata.name(letter)}")
+    except ValueError:
+        print(f"{letter}: {ord(letter)} \\u{ord(letter):04x} -----")
